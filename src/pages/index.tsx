e11046efc9a9318bad81997e686e80045360366f
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2021-05-22 16:30:55 +0200

    Changed text sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
 
             <div
                 className={"h-screen w-full text-white flex flex-col bg-gray-800 items-center justify-center text-center"}>
-                <div className={""}>
-                    <div className={"jetbrains font-light lg:text-9xl text-4xl"}>We are</div>
-                    <div className={"jetbrains font-light lg:text-9xl text-4xl mb-2"}>GenericDevelopment</div>
-                    <div className={"jetbrains font-light text-1xl mb-8"}>An opensource development company which strives to build and improve tech</div>
+                <div className={"m-2"}>
+                    <div className={"jetbrains font-light 2xl:text-9xl xl:text-8xl lg:text-7xl md:text-6xl sm:text-4xl text-3xl"}>We are</div>
+                    <div className={"jetbrains font-light 2xl:text-9xl xl:text-8xl lg:text-7xl md:text-6xl sm:text-4xl text-3xl mb-2"}>GenericDevelopment</div>
+                    <div className={"jetbrains font-light md:text-1xl text-xs mb-8"}>An opensource development company which strives to build and improve tech</div>
                 </div>
                 {/*<div className={"flex flex-col"}>*/}
                 {/*    <p className={"mb-2"}>In the meantime if you are intrested my portfolio while I am building this site</p>*/}
